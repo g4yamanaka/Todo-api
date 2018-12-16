70fa9cd5d51431e3743e37d544380cfb4a708c93
g4yamanaka <g4.yamanaka@gmail.com>
2018-12-16 14:22:38 +0900

    データベースをH2からMySqlへ変更

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -6,31 +6,31 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   user_id   BIGINT AUTO_INCREMENT,
   user_name VARCHAR(255),
-  PRIMARY KEY (user_id),
-);
+  PRIMARY KEY (user_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE tabs (
-  tab_id      BIGINT PRIMARY KEY AUTO_INCREMENT,
+  tab_id      BIGINT AUTO_INCREMENT,
   tab_name    VARCHAR(255),
   user_id  BIGINT,
   PRIMARY KEY (tab_id),
   CONSTRAINT FK__tabs__user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE projects (
-  project_id    BIGINT PRIMARY KEY AUTO_INCREMENT,
+  project_id    BIGINT AUTO_INCREMENT,
   project_name  VARCHAR(255),
   tab_id       BIGINT,
   PRIMARY KEY (project_id),
   CONSTRAINT FK__projects__tab_id FOREIGN KEY (tab_id) REFERENCES tabs (tab_id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE tasks (
-  task_id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+  task_id         BIGINT AUTO_INCREMENT,
   task_content    VARCHAR(255),
   created_time    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   completed_flag  BOOLEAN DEFAULT FALSE,
   project_id      BIGINT,
   PRIMARY KEY (task_id),
   CONSTRAINT FK__tasks__project_id FOREIGN KEY (project_id) REFERENCES projects (project_id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/main/resources/db/migration/V2__import-initial-data.sql
+++ src/main/resources/db/migration/V2__import-initial-data.sql
@@ -7,14 +7,14 @@ INSERT INTO tabs (tab_id, tab_name, user_id) VALUES (101, 'GTD', 1);
 INSERT INTO tabs (tab_id, tab_name, user_id) VALUES (102, 'TODO', 1);
 INSERT INTO tabs (tab_id, tab_name, user_id) VALUES (103, 'SHOP', 2);
 
---projects data
+-- projects data
 INSERT INTO projects (project_id, project_name, tab_id) VALUES (201, 'inbox', 101);
 INSERT INTO projects (project_id, project_name, tab_id) VALUES (202, '後でやる', 101);
 INSERT INTO projects (project_id, project_name, tab_id) VALUES (203, 'callender', 102);
 INSERT INTO projects (project_id, project_name, tab_id) VALUES (204, 'callender', 103);
 
 
---tasks data
+-- tasks data
 INSERT INTO tasks (task_id, task_content, completed_flag, project_id) VALUES (301, '牛乳を買う', TRUE, 201);
 INSERT INTO tasks (task_id, task_content, completed_flag, project_id) VALUES (302, '本を買う', FALSE, 201);
 INSERT INTO tasks (task_id, task_content, completed_flag, project_id) VALUES (303, 'ボーリングをする', FALSE, 202);
